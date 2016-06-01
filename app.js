b0e9bae93f3e9f85ce4c0b3f72ba5440bec828c4
maddy <maddyraj65@gmail.com>
2016-06-01 10:31:46 +0530

    Initial Files
    Initial Files during App generation

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'LoginApp',
+
+    extend: 'app1.Application',
+    
+   // autoCreateViewport: 'app1.view.main.Main',
+	requires:[
+			'LoginApp.view.login.Login'//,
+           // 'app1.view.main.Main',
+			]
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to app1.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
